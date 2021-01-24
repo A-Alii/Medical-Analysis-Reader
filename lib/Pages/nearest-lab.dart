a5b90fa290851e0083b8668dcb676655bb77645a
Hossam-elemam <hossahmed151@gmail.com>
2021-01-24 21:20:49 +0200

    new features

--- lib/Pages/nearest-lab.dart
+++ lib/Pages/nearest-lab.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:medical/Components/mydrawer.dart';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:search_map_place/search_map_place.dart';
+import 'package:url_launcher/url_launcher.dart';
 class Nearest extends StatefulWidget {
   @override
   _NearestState createState() => _NearestState();
@@ -23,10 +28,10 @@ class _NearestState extends State<Nearest> {
         title: Text("Nearest Lab"),
         centerTitle: true,
       ),
-      //drawer: MyDrawer(),
-      body: Container(
-        child: Text("Nearest Lab"),
-      ),
+      drawer: MyDrawer(),
+     body: RaisedButton(
+       child: Text("Open Nearest Labs" ) , color: Colors.blueAccent,
+       onPressed: (){openurl();},),
 
       //Navigation
       bottomNavigationBar: BottomNavigationBar(
@@ -63,5 +68,100 @@ class _NearestState extends State<Nearest> {
         ],
       ),
     );
+
+  }
+
+   openurl() async{
+    const url1 ='https://www.google.com/maps/search/?api=1&query=medical+labs';
+    if (await canLaunch(url1)) {
+      await launch(url1);
+    } else {
+      throw 'Could not launch $url1';
+    }
   }
 }
+
+
+// class _NearestState extends State<Nearest>{
+//
+//   Completer<GoogleMapController> _controller = Completer();
+//   static const LatLng _center = const LatLng(45.521563, -122.677433);
+//   final Set<Marker> _markers = {};
+//   LatLng _lastMapPosition = _center;
+//   MapType _cutrrentMapType = MapType.normal;
+//
+//   _onMapCreated(GoogleMapController controller) {
+//     _controller.complete(controller);
+//   }
+//
+//   _onCameraMove(CameraPosition position) {
+//     _lastMapPosition = position.target;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: <Widget>[
+//           IconButton(
+//             icon: Icon(Icons.search),
+//             onPressed: () {},
+//           )
+//         ],
+//       ),
+//       body: Stack(
+//         children: <Widget>[
+//           GoogleMap(
+//             onMapCreated: _onMapCreated,
+//             initialCameraPosition: CameraPosition(
+//               target: _center,
+//               zoom: 11.0,
+//             ),
+//             mapType: _cutrrentMapType,
+//             markers: _markers,
+//             onCameraMove: _onCameraMove,
+//           ),
+//         ],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         type: BottomNavigationBarType.fixed,
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(
+//               Icons.camera_alt,
+//             ),
+//             title: Text('Scan'),
+//             backgroundColor: Colors.blue,
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(
+//               Icons.location_pin,
+//             ),
+//             title: Text('Nearesrt'),
+//             backgroundColor: Colors.blue,
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(
+//               Icons.open_in_new_outlined,
+//             ),
+//             title: Text('compare'),
+//             backgroundColor: Colors.blue,
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(
+//               Icons.feedback,
+//             ),
+//             title: Text('feedback'),
+//             backgroundColor: Colors.blue,
+//           ),
+//         ],
+//       ),
+//     );
+//
+//
+//
+//   }
+//
+//
+// }
--- lib/Pages/notes-on-analysis.dart
+++ lib/Pages/notes-on-analysis.dart
@@ -9,34 +9,24 @@ class Notes extends StatefulWidget {
   _NotesState createState() => _NotesState();
 }
 
+
+
 class _NotesState extends State<Notes> {
-  List<Item> _items = <Item>[
-    Item(header: "  Haematocrit", body: "Body0"),
-    Item(header: "  MCV", body: "Body1"),
-    Item(header: "  Haemoglobin", body: "Body3"),
-  ];
   @override
   Widget build(BuildContext context) {
-    return _buildPage();
-  }
-
-  Widget _buildPage() {
-    return SafeArea(
-      top: true,
-      child: Scaffold(
-        backgroundColor: Colors.white,
+    return new SafeArea(
+    top: true,
+      child: new Scaffold(
         appBar: AppBar(
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
+                icon: Icon(Icons.search),
+                onPressed: (){},
             )
           ],
           title: Text("Notes on analysis"),
           centerTitle: true,
         ),
-        // drawer: MyDrawer(),
-
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
           items: [
@@ -70,86 +60,236 @@ class _NotesState extends State<Notes> {
             ),
           ],
         ),
-
         body: Padding(
           padding: EdgeInsets.only(top: 25.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              // Spacer(),
-              Expanded(
-                child: _buildList(),
-              )
+            children: [
+              Expanded(child: _buildList(),
+              ),
+
             ],
           ),
         ),
-      ),
-    );
+        ),
+
+      );
   }
+
+
 
   Widget _buildList() {
-    return ListView(
-      children: <Widget>[
-        ExpansionPanelList(
-          expansionCallback: (int index, bool isExtended) {
-            setState(() {
-              _items[index].isExpanded = !_items[index].isExpanded;
-            });
-          },
-          children: _items.map((Item item) {
-            return ExpansionPanel(
-              headerBuilder: (BuildContext context, bool isExpanded) {
-                return Text(item.header);
-              },
-              isExpanded: item.isExpanded,
-              body: Container(color: Colors.white, child: Text(item.body)),
-            );
-          }).toList(),
-        ),
-        IconButton(
-          icon: Icon(Icons.volume_up),
-          onPressed: () {},
-        ),
-        const Divider(
-          color: Colors.grey,
-          height: 40,
-          thickness: 2,
-          indent: 40,
-          endIndent: 40,
-        ),
-        new Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20.0),
-          padding: const EdgeInsets.all(5.0),
-          decoration:
-              BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-          child: Text(
-            "Analyzes required",
-            style: TextStyle(
-              fontSize: 15.0,
+    return ListView.builder(
+      itemCount: vehicles.length,
+      itemBuilder: (context, i) {
+        return new ExpansionTile(
+          title: new Text(vehicles[i].title, style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),),
+          children: <Widget>[
+            new Column(
+              children: _buildExpandableContent(vehicles[i]),
             ),
-          ),
-        ),
-        new Container(
-          margin: const EdgeInsets.all(15.0),
-          padding: const EdgeInsets.all(3.0),
-          decoration:
-              BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-          child: Text(
-            "Dr Recommanded",
-            style: TextStyle(
-              fontSize: 15.0,
-            ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
+  }
+
+
+
+  _buildExpandableContent(Vehicle vehicle) {
+    List<Widget> columnContent = [];
+
+    for (String content in vehicle.contents)
+      columnContent.add(
+        new ListTile(
+          title: new Text(content, style: new TextStyle(fontSize: 18.0), ),
+          leading: new IconButton(icon: Icon(vehicle.icon), onPressed: (){}),
+        ),
+      );
+
+    return columnContent;
   }
 }
 
-class Item {
-  Item({this.body, this.header, this.isExpanded = false});
+class Vehicle {
+  final String title;
+  List<String> contents = [];
+  final IconData icon;
 
-  final String body;
-  final String header;
-  bool isExpanded;
+  Vehicle(this.title, this.contents, this.icon );
 }
+
+List<Vehicle> vehicles = [
+  new Vehicle(
+    'Heamatocrit',
+    ['Body 0'],
+    Icons.volume_up,
+  ),
+  new Vehicle(
+    'MCV',
+    ['Body 1'],
+    Icons.volume_up,
+    ),
+  new Vehicle(
+    'Haemoglobin',
+    ['Body 3'],
+    Icons.volume_up,
+  ),
+
+new Vehicle(
+'Analyzes Required',
+['Text'],
+Icons.volume_up,
+),
+
+new Vehicle(
+'Dr Recommended',
+['Text'],
+Icons.volume_up,
+),
+];
+
+
+// class _NotesState extends State<Notes> {
+//   List<Item> _items = <Item>[
+//     Item(header: "  Haematocrit", body: "Body0"),
+//     Item(header: "  MCV", body: "Body1"),
+//     Item(header: "  Haemoglobin", body: "Body3"),
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return _buildPage();
+//   }
+//
+//   Widget _buildPage() {
+//     return SafeArea(
+//       top: true,
+//       child: Scaffold(
+//         backgroundColor: Colors.white,
+//         appBar: AppBar(
+//           actions: <Widget>[
+//             IconButton(
+//               icon: Icon(Icons.search),
+//               onPressed: () {},
+//             )
+//           ],
+//           title: Text("Notes on analysis"),
+//           centerTitle: true,
+//         ),
+//         // drawer: MyDrawer(),
+//
+//         bottomNavigationBar: BottomNavigationBar(
+//           type: BottomNavigationBarType.fixed,
+//           items: [
+//             BottomNavigationBarItem(
+//               icon: Icon(
+//                 Icons.camera_alt,
+//               ),
+//               title: Text('Scan'),
+//               backgroundColor: Colors.blue,
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(
+//                 Icons.location_pin,
+//               ),
+//               title: Text('Nearesrt'),
+//               backgroundColor: Colors.blue,
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(
+//                 Icons.open_in_new_outlined,
+//               ),
+//               title: Text('compare'),
+//               backgroundColor: Colors.blue,
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(
+//                 Icons.feedback,
+//               ),
+//               title: Text('feedback'),
+//               backgroundColor: Colors.blue,
+//             ),
+//           ],
+//         ),
+//
+//         body: Padding(
+//           padding: EdgeInsets.only(top: 25.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: <Widget>[
+//               // Spacer(),
+//               Expanded(
+//                 child: _buildList(),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildList() {
+//     return ListView(
+//       children: <Widget>[
+//         ExpansionPanelList(
+//           expansionCallback: (int index, bool isExtended) {
+//             setState(() {
+//               _items[index].isExpanded = !_items[index].isExpanded;
+//             });
+//           },
+//           children: _items.map((Item item) {
+//             return ExpansionPanel(
+//               headerBuilder: (BuildContext context, bool isExpanded) {
+//                 return Text(item.header);
+//               },
+//               isExpanded: item.isExpanded,
+//               body: Container(color: Colors.white, child: Text(item.body)),
+//             );
+//           }).toList(),
+//         ),
+//         IconButton(
+//           icon: Icon(Icons.volume_up),
+//           onPressed: () {},
+//         ),
+//         const Divider(
+//           color: Colors.grey,
+//           height: 40,
+//           thickness: 2,
+//           indent: 40,
+//           endIndent: 40,
+//         ),
+//         new Container(
+//           margin: const EdgeInsets.symmetric(horizontal: 20.0),
+//           padding: const EdgeInsets.all(5.0),
+//           decoration:
+//               BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+//           child: Text(
+//             "Analyzes required",
+//             style: TextStyle(
+//               fontSize: 15.0,
+//             ),
+//           ),
+//         ),
+//         new Container(
+//           margin: const EdgeInsets.all(15.0),
+//           padding: const EdgeInsets.all(3.0),
+//           decoration:
+//               BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+//           child: Text(
+//             "Dr Recommanded",
+//             style: TextStyle(
+//               fontSize: 15.0,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+//
+// class Item {
+//   Item({this.body, this.header, this.isExpanded = false});
+//
+//   final String body;
+//   final String header;
+//   bool isExpanded;
+// }
